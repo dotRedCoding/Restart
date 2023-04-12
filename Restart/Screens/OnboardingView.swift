61676c44e2a53a2f15a908c0f180e0141f5ac58b
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-04-12 15:40:51 -0400

    - custom slider button is now functionng - when the user slides the button halfway or the button width / 2 it will either snap back to an offset of 0 (the start) or if the offset is greater than halfway it will navigate the user to the HomeView screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -11,6 +11,8 @@ struct OnboardingView: View {
     
     //MARK: - PROPERTY
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
     
     //MARK: - BODY
     
@@ -69,7 +71,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                     }
                     // 4. CIRCLE (DRAGGABLE)
@@ -82,18 +84,34 @@ struct OnboardingView: View {
                                 .padding(8)
                             Image(systemName: "chevron.right.2")
                                 .font(.system(size: 24, weight: .bold))
-                        }
+                        } // END of Zstack
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                    
+                                    buttonOffset = 0
+                                }
+                        ) // End of Gesture
                         
                         Spacer()
                     } // END OF HSTACK
                     
                 } // END OF FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
                 
             } // End of VSTACK
