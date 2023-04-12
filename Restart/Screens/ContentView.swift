ab6c59f81e16fadeb64b62d1e1971bfe4cac5361
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-04-12 13:33:15 -0400

    - Initial Commit - File set up and assets are all imported for; app icon, sounds and the app color - Initial views set up and can navigate back and forth using buttons that change the AppState

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Jared on 2023-04-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // @AppStorage is a property wrapper - purpose is store the value using {get set}
+    // "onboarding" is the User Defaults Key
+    // "isOnboardingViewActive" is the actual proptery name
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,33 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Jared on 2023-04-12.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    //MARK: - PROPERTY
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    
+    //MARK: - BODY
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Home")
+                .font(.largeTitle)
+            Button(action: {
+                isOnboardingViewActive = true
+            }) {
+                Text("Restart")
+            }
+        } // VSTACK
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
